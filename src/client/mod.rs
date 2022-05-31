fe1b5984f774829219fc84c0a20ea3f6c311b400
tacogips <me@tacogips.me>
2022-05-31 11:35:39 +0900

    pub error

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,4 +1,4 @@
-mod error;
+pub mod error;
 pub mod kernel;
 pub mod types;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod client;
+pub use client::error::*;
 pub use client::kernel::*;
 pub use client::types::*;
 pub use client::*;
